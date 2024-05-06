fb1fe2310b2b35a1ea004873ae26bc1c43ce5ed9
Cirrous <christianraisch@gmail.com>
2024-05-06 18:17:13 +0200

    deleted unnecessary files, small changes to text and icons

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="/src/components/icons/html_icon.png" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/game">Game</RouterLink>
       </nav>
     </div>
   </header>
