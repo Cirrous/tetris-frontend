c5fd718647a0d890ca4de0ee0e422e2a8cc71b19
Cirrous <christianraisch@gmail.com>
2024-05-07 13:46:06 +0200

    created leaderboard (filled with test data)

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="/src/components/icons/html_icon.png" width="125" height="125" />
+    <img alt="Tetris logo" class="logo" src="/src/components/icons/tetris_logo.png" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
---
+++ src/components/Leaderboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Leaderboard</h1>
+    <input type="number" v-model="newHighscore" placeholder="Type in your highscore">
+    <button @click="addHighscore">Add</button>
+
+    <ul>
+      <li v-for="(score, index) in highscores" :key="index">{{ score }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      highscores: [1000, 800, 600, 400, 200], // Beispiel-Highscore-Liste
+      newHighscore: null
+    };
+  },
+  methods: {
+    addHighscore() {
+      if (this.newHighscore !== null && !isNaN(this.newHighscore)) {
+        this.highscores.push(parseInt(this.newHighscore));
+        this.highscores.sort((a, b) => b - a);
+        this.newHighscore = null;
+      } else {
+        alert('Please enter a valid number!');
+      }
+    }
+  }
+};
+</script>
